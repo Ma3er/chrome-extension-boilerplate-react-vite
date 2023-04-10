eb01d54e0351c7c97cc8af64b71927954d4e3f25
Ma3er <13718570+Ma3er@users.noreply.github.com>
2023-04-10 20:51:21 +0100

    Getting a array after save

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,24 +1,42 @@
 import React from "react";
-import logo from "@assets/img/logo.svg";
-import "@pages/popup/Popup.css";
 
-const Popup = () => {
+interface Snippet {
+  id: number;
+  code1: string;
+  code2: string;
+}
+
+const snippets: Snippet[] = [];
+
+const handleSaveClick = () => {
+  const input1 = document.getElementById("input1") as HTMLInputElement | null;
+  const input2 = document.getElementById("input2") as HTMLInputElement | null;
+
+  if (input1 && input2) {
+    const snippet: Snippet = {
+      id: snippets.length,
+      code1: input1.value,
+      code2: input2.value,
+    };
+
+    snippets.push(snippet);
+    input1.value = "";
+    input2.value = "";
+
+    console.log(snippets);
+  }
+};
+
+// add a list of snippets to the popup
+
+const Popup: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+    <div>
+      <input type="text" id="input1" placeholder="Name" />
+      <input type="text" id="input2" placeholder="Code" />
+      <button id="save-btn" onClick={handleSaveClick}>
+        Save
+      </button>
     </div>
   );
 };
