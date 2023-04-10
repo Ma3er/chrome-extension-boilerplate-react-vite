b70ddf30e19eb77a8c72d08cb9a1c5d06f76a429
Ma3er <13718570+Ma3er@users.noreply.github.com>
2023-04-10 22:30:27 +0100

    working with chrome storage

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,42 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Popup.css";
 
 interface Snippet {
   id: number;
+  title: string;
   code1: string;
   code2: string;
 }
 
-const snippets: Snippet[] = [];
-
-const handleSaveClick = () => {
-  const input1 = document.getElementById("input1") as HTMLInputElement | null;
-  const input2 = document.getElementById("input2") as HTMLInputElement | null;
-
-  if (input1 && input2) {
-    const snippet: Snippet = {
-      id: snippets.length,
-      code1: input1.value,
-      code2: input2.value,
-    };
-
-    snippets.push(snippet);
-    input1.value = "";
-    input2.value = "";
-
-    console.log(snippets);
-  }
-};
-
-// add a list of snippets to the popup
-
 const Popup: React.FC = () => {
+  const [snippets, setSnippets] = useState<Snippet[]>([]);
+
+  useEffect(() => {
+    chrome.storage.local.get("snippets", (data) => {
+      if (data.snippets) {
+        setSnippets(data.snippets);
+      }
+    });
+  }, []);
+
+  const handleSaveClick = () => {
+    const input1 = document.getElementById("input1") as HTMLInputElement | null;
+    const input2 = document.getElementById("input2") as HTMLInputElement | null;
+
+    if (input1 && input2) {
+      const snippet: Snippet = {
+        id: snippets.length,
+        code1: input1.value,
+        code2: input2.value,
+      };
+
+      setSnippets((prevSnippets) => [...prevSnippets, snippet]);
+      input1.value = "";
+      input2.value = "";
+
+      chrome.storage.local.set({ snippets: snippets.concat(snippet) });
+    }
+  };
+
+  const handleClearClick = () => {
+    setSnippets([]);
+    chrome.storage.local.clear();
+  };
+
   return (
     <div>
-      <input type="text" id="input1" placeholder="Name" />
-      <input type="text" id="input2" placeholder="Code" />
+      <input type="text" id="input1" placeholder="Enter code snippet 1" />
+      <input type="text" id="input2" placeholder="Enter code snippet 2" />
       <button id="save-btn" onClick={handleSaveClick}>
         Save
       </button>
+      <button id="clear-btn" onClick={handleClearClick}>
+        Clear
+      </button>
+      <ul>
+        {snippets.map((snippet) => (
+          <li key={snippet.id}>
+            <h3>{snippet.title}</h3>
+            <pre>{snippet.code1}</pre>
+            <pre>{snippet.code2}</pre>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
